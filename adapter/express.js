4f379eb8829877ec73dcc76f633305c74fd88250
波影 <liming.mlm@alibaba-inc.com>
2014-11-07 16:58:08 +0800

    express

--- adapter/express.js
+++ adapter/express.js
@@ -1,6 +1,15 @@
 module = module.exports = function(param, flag) {
     var FlexCombo = require("../flex-combo");
-    var fcInst = null;
+    var fcInst = new FlexCombo(param, flag);
+
+    /*
+    fcInst.defineParser(function(url) {
+        return [];
+    });
+    fcInst.addEngine("\\.suffix$", function(absPath, url) {
+        return "";
+    });
+    */
 
     return function(req, res, next) {
         fcInst = new FlexCombo(param, flag);
