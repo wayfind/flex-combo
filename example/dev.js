c3a9d6b44d0946fa54f58d9aa460dd4a3651cdf8
limingv5 <limingv5@gmail.com>
2015-03-10 20:31:13 +0800

    readme

--- example/dev.js
+++ example/dev.js
@@ -9,9 +9,25 @@ var API  = require("../").API;
 var fcInst = new API({});
 
 // 自定义URL解析规则
-//fcInst.defineParser(function (url) {
-//  return [url];
-//});
+fcInst.defineParser(function (_url) {
+  var urlLib = require("url");
+  var pathLib = require("path");
+
+  var url = urlLib.parse(_url).path.replace(/\\|\/{1,}/g, '/');
+  var prefix = url.indexOf(this.param.servlet + '?');
+
+  if (prefix != -1) {
+    var base = url.slice(0, prefix);
+    var file = url.slice(prefix + this.param.servlet.length + 1);
+    var filelist = file.split(this.param.seperator, 1000);
+    return filelist.map(function (i) {
+      return pathLib.join(base, i).replace(/\\|\/{1,}/g, '/');
+    });
+  }
+  else {
+    return [url];
+  }
+});
 
 // 添加assets动态编译引擎
 // 例如要加入stylus支持，可通过addEngine添加动态编译逻辑
